ad72c2876ad937b8be82b14069d8362fc3295c21
Micah Anderson <micah.anderson@dci-student.org>
2024-01-23 14:24:16 +0100

    Tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
+    <div className='App'>
+      <Home />
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-red-600 font-extrabold underline">Home</h1>
+    </div>
+  );
+}
+
+export default Home
