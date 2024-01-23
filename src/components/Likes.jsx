1d017e1f0ed036d0a1d1dc02560d354d5f23064b
Micah Anderson <micah.anderson@dci-student.org>
2024-01-23 13:43:49 +0100

    First commit. Set up project and create components in base form

---
+++ src/components/Likes.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Likes = () => {
+  return (
+    <div>
+      <h1>Heres a list of random things I like and enjoy in life</h1>
+      <ul>
+        {/* Have buttons you click and they ruote to the thing */}
+        <h2>What:</h2>
+        <li>Youtube</li>
+        <h3>Why: Because you can learn basically anything.</h3>
+        <p>
+          Specifically because:There's so from so many incredible creators, all
+          with unique styles and expressions of themselves.
+        </p>
+        <h3>Recommendations: </h3>
+        <ul>
+          <li>Nisat & his channel: "The Spirited Man"</li>
+          <li>Support him on patreon at...</li>
+        </ul>
+        <ul>
+          <li>repeat</li>
+          <li>etc</li>
+        </ul>
+      </ul>
+    </div>
+  );
+}
+
+export default Likes
